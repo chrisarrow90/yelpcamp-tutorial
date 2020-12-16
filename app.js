15bbcd2b7ca61cf7b0fa3623fa309e8750483f1b
chrisarrow90 <chrisarrowsmith@gmail.com>
2020-12-17 00:56:43 +1300

    <build>: setup dev mongodb connection using mongoose

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 const { resolveSoa } = require('dns');
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
+
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useCreateIndex: true,
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('Database connected');
+});
 
 app.get('/', (req, res) => {
   res.render('home');
