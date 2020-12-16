f2c8cd5bdde3a42785af972b7c09e2d80575c2c5
chrisarrow90 <chrisarrowsmith@gmail.com>
2020-12-17 01:50:34 +1300

    <build>: add GET route handler to render a form to  create a new campground. add POST route handler to handle POST data from form and add new campground to the database

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.urlencoded({ extended: true }));
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
   useUnifiedTopology: true,
@@ -30,9 +31,19 @@ app.get('/campgrounds', async (req, res) => {
   res.render('campgrounds/index', { campgrounds });
 });
 
+app.get('/campgrounds/new', (req, res) => {
+  res.render('campgrounds/new');
+});
+
 app.get('/campgrounds/:id', async (req, res) => {
   const campground = await Campground.findById(req.params.id);
   res.render('campgrounds/show', { campground });
+});
+
+app.post('/campgrounds', async (req, res) => {
+  const campground = new Campground(req.body.campground);
+  await campground.save();
+  res.redirect(`/campgrounds/${campground._id}`);
 });
 
 app.listen(3000, () => {
