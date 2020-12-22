fa2b6130a7e31ff6697b9fbb5fc53cc5daca2d3b
chrisarrow90 <chrisarrowsmith@gmail.com>
2020-12-22 22:40:42 +1300

    <build>: add authorisation for review deletion. Add reference to review author on the reviewSchema

--- routes/reviews.js
+++ routes/reviews.js
@@ -3,16 +3,19 @@ const router = express.Router({ mergeParams: true }); // reqd to get params from
 const catchAsync = require('../utils/catchAsync');
 const Campground = require('../models/campground');
 const Review = require('../models/review');
-const { isValidReview } = require('../middleware.js');
+const { isValidReview, isLoggedIn, isReviewAuthor } = require('../middleware.js');
 
 router.post(
   '/',
+  isLoggedIn,
   isValidReview,
   catchAsync(async (req, res) => {
     // find associated campground
     const campground = await Campground.findById(req.params.id);
     // create new review instance
     const review = new Review(req.body.review);
+    // assign the id of the logged in user to the review
+    review.author = req.user._id;
     // add review into the found campground document
     campground.reviews.push(review);
     // save both review and campground docs
@@ -25,6 +28,8 @@ router.post(
 
 router.delete(
   '/:reviewId',
+  isLoggedIn,
+  isReviewAuthor,
   catchAsync(async (req, res) => {
     const { id, reviewId } = req.params;
     await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
