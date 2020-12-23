90f058e21c5b34e8a9638b8e76a7f9250c2aacc2
chrisarrow90 <chrisarrowsmith@gmail.com>
2020-12-24 12:48:50 +1300

    <fix>: mapbox not showing on mobile. Made changes to Content Security Policiy to hopefully address this.

--- app.js
+++ app.js
@@ -53,29 +53,28 @@ db.once('open', () => {
 
 // Helmet Configuration
 const scriptSrcUrls = [
-  'https://stackpath.bootstrapcdn.com/',
-  'https://api.tiles.mapbox.com/',
-  'https://api.mapbox.com/',
-  'https://kit.fontawesome.com/',
-  'https://cdnjs.cloudfare.com/',
-  'https://cdn.jsdelivr.net/',
+  'https://stackpath.bootstrapcdn.com',
+  'https://api.tiles.mapbox.com',
+  'https://api.mapbox.com',
+  'https://kit.fontawesome.com',
+  'https://cdnjs.cloudfare.com',
+  'https://cdn.jsdelivr.net',
 ];
 
 const styleSrcUrls = [
-  'https://kit-free.fontawesome.com/',
-  'https://stackpath.bootstrapcdn.com/',
+  'https://kit-free.fontawesome.com',
+  'https://stackpath.bootstrapcdn.com',
   'https://cdn.jsdelivr.net',
-  'https://api.mapbox.com/',
-  'https://api.tiles.mapbox.com/',
-  'https://fonts.googleapis.com/',
-  'https://use.fontawesome.com/',
+  'https://api.mapbox.com',
+  'https://api.tiles.mapbox.com',
+  'https://fonts.googleapis.com',
+  'https://use.fontawesome.com',
 ];
 
 const connectSrcUrls = [
-  'https://api.mapbox.com/',
-  'https://a.tiles.mapbox.com/',
-  'https://b.tiles.mapbox.com/',
-  'https://events.mapbox.com/',
+  'https://api.mapbox.com',
+  'https://*.tiles.mapbox.com',
+  'https://events.mapbox.com',
 ];
 
 const fontSrcUrls = [];
